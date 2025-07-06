6ea04da9bf47ea4b8d1efaa2ae3c1e759dc95f42
LatidoIA <sebaurream@gmail.com>
2025-07-06 00:37:00 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,16 @@
 # db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from models import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./latido.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+# Inicializa las tablas
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
